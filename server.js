6ab3dffa66e6dacea97bfe8f56b658810105d277
TUNG TUNG OUYANG <ouyangtt@TUNGs-MacBook-Air.local>
2018-03-02 21:20:36 -0800

    adding gitignore and inti server.js

--- server.js
+++ server.js
@@ -1,1 +1,33 @@
+// Require npm packages
 var express = require("express");
+var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
+
+// Set up for express
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// Set up for express app to handle data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+app.use(methodOverride('_method'));
+
+// Set up for handlebars
+var exphbs = require("express-handlebars");
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+app.use(express.static('public'));
+
+// Import routes and give the server access to them.
+var routes = require("./controllers/burger_controller.js");
+
+app.use("/", routes);
+
+// Starts the server to begin listening
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
